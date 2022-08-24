29395838c945c00909555b9f1ddaf61407536703
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-24 20:39:57 +0530

    resolved errors

---
+++ fe/src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import auth_router from "./modules/auth_router";
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    ...auth_router
+  ]
+})
---
+++ fe/src/views/auth/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <form class="box">
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" type="email" placeholder="e.g. alex@example.com">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="********">
+        </div>
+      </div>
+
+      <button class="button is-primary">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
